c48de5a7ae9332e32620b811a17c84fd0d1e7392
Eko Edita <ekoeditaa@gmail.com>
2019-08-07 00:02:37 +0800

    Update new version
    Signed-off-by: Eko Edita <ekoeditaa@gmail.com>

---
+++ precache-manifest.dd17250231fc26e0460c527a03ae2ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b34e2ddc57d61b9d4b86c5fa6a88e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36431ca7f880d165c5f2",
+    "url": "/static/css/main.402ecab3.chunk.css"
+  },
+  {
+    "revision": "9b3c2afba8a5232e4add",
+    "url": "/static/js/2.24e6aafa.chunk.js"
+  },
+  {
+    "revision": "36431ca7f880d165c5f2",
+    "url": "/static/js/main.8ba200d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
